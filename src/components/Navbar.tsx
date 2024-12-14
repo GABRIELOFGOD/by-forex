8260b831703d657640c10501223f72807a0d8471
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-12-14 14:38:34 +0100

    OUR GOD IS ALWAYS MERCIFUL

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,108 +1,18 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useEffect } from "react";
-import toast from "react-hot-toast";
-import { useParams } from "react-router-dom";
-import { useAccount, useDisconnect, useReadContract, useWriteContract } from "wagmi";
-import { byForexConfig } from "../../abi";
-
-// const isValidAddress = (address: string | undefined): boolean => {
-//   return /^0x[a-fA-F0-9]{40}$/.test(address || '');
-// };
-
-type UserInfo = [
-  string,    // walletAddress
-  bigint,    // currentPackageLevel
-  bigint,    // totalInvestment  
-  bigint,    // directBusinessVolume
-  bigint,    // directReferralCount
-  string,    // upline
-  bigint,    // totalEarnings
-  boolean,   // isRegistered
-  bigint,    // totalWithdrawal
-  bigint,    // highestPackage
-  bigint,    // totalBalance
-  bigint     // poolEarnings
-]
 
 const Navbar = () => {
-  const { address: referralAddress } = useParams();
-  const { isConnected, address } = useAccount();
-  const { disconnect } = useDisconnect();
-  const { data: hash, writeContract, error } = useWriteContract()
-
-  const handleRegister = () => {
-    // if (!isValidAddress(referralAddress)) {
-    //   return
-    // }
-    writeContract({
-      abi: byForexConfig.abi,
-      address: byForexConfig.address as `0x${string}`,
-      functionName: "registerUser",
-      args: ["0x7b49660dc6F25326d2fA7C3CD67970dF73eB5Ec1"], // admin line
-    });
-  };
-
-  const { data: userInfo } = useReadContract({
-    abi: byForexConfig.abi,
-    address: byForexConfig.address as `0x${string}`,
-    functionName: 'users',
-    args: [address],
-  }) as { data: UserInfo }
-
-  useEffect(() => {
-    console.log(referralAddress)
-    if (!isConnected) {
-      return;
-    }
-    if (userInfo && userInfo[0] === "0x0000000000000000000000000000000000000000") {
-      handleRegister();
-    }
-  }, [userInfo]);
-
-
-  useEffect(() => {
-    if (hash) {
-      console.log("Transaction hash:", hash);
-      toast.success(
-        <div>
-          Transaction sent!
-          <a
-            href={`https://testnet.bscscan.com/tx/${hash}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 underline ml-1"
-          >
-            View on BscScan
-          </a>
-        </div>
-      );
-    }
-  }, [hash]);
-
-  useEffect(() => {
-    if (error) {
-      toast.error(error.message)
-    }
-  }, [error])
-
   return (
-    <div className="fixed top-0 w-full flex justify-between py-2 px-3 md:py-10 md:px-28 backdrop-blur-md z-40">
+    <div className="fixed top-0 w-full flex justify-between py-2 px-3 md:py-4 md:px-28 backdrop-blur-md z-40">
       <div className="my-auto">
         <div className="text-2xl md:text-4xl my-auto font-bold">
           <span className="text-primary">By</span><span className="text-white">Forex</span>
         </div>
       </div>
       <div className="gap-2 flex">
-        {
-          isConnected ? (
-            <button onClick={() => disconnect()} className="outline-none text-white scale-[1.01] rounded-full px-8 py-2 bg-primary">Disconnect</button>
-          ) : (
-            <ConnectButton />
-          )
-        }
+        <ConnectButton />
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Investments.tsx
+++ src/pages/Investments.tsx
@@ -1,134 +1,5 @@
-import { useEffect, useState } from "react";
-import toast from 'react-hot-toast';
-import { parseAbi } from "viem";
-import { useAccount, useReadContract, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
-import { byForexConfig } from "../../abi";
-
 const packages = [20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480, 40960]
-const USDT_ADDRESS = '0x93323bB3896C5eff97320BC63E4FbccB41D0C8C4';
-
-interface DashboardInfo {
-  availableIncomeClaim: bigint;
-  poolsClaim: bigint[];
-  isPoolEligible: boolean[];
-  totalIncomeClaim: bigint;
-}
-
-// Format BigInt to regular number with commas
-const formatAmount = (amount: number) => {
-  return (Number(amount)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-const formatBigInt = (amount: number | bigint) => {
-  return (Number(amount) / 1e18).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-
-type UserInfo = [
-  string,    // walletAddress
-  bigint,    // currentPackageLevel
-  bigint,    // totalInvestment  
-  bigint,    // directBusinessVolume
-  bigint,    // directReferralCount
-  string,    // upline
-  bigint,    // totalEarnings
-  boolean,   // isRegistered
-  bigint,    // totalWithdrawal
-  bigint,    // highestPackage
-  bigint,    // totalBalance
-  bigint     // poolEarnings
-]
-
 const Investments = () => {
-  const [isApproved, setIsApproved] = useState(false);
-  const [investmentAmount, setInvestmentAmount] = useState(0);
-  const [packageId, setPackageId] = useState(0);
-  const { address } = useAccount()
-  const { data: hash, writeContract, error } = useWriteContract()
-  const { isLoading: isConfirming } =
-    useWaitForTransactionReceipt({
-      hash,
-    })
-
-  const { data: dashInfo } = useReadContract({
-    abi: byForexConfig.abi,
-    address: byForexConfig.address as `0x${string}`,
-    functionName: 'getUserDashboardInfo',
-    args: [address],
-  }) as { data: DashboardInfo }
-
-  const { data: userInfo } = useReadContract({
-    abi: byForexConfig.abi,
-    address: byForexConfig.address as `0x${string}`,
-    functionName: 'users',
-    args: [address],
-  }) as { data: UserInfo }
-
-  const generateReferralLink = () => {
-    return `byForex.app/register/${address}`;
-  };
-
-  useEffect(() => {
-    if (error) {
-      console.log(error);
-      toast.error("Error: " + error.message);
-    }
-  }, [error])
-
-  useEffect(() => {
-    if (hash) {
-      toast.success(
-        <div>
-          Transaction sent!
-          <a
-            href={`https://testnet.bscscan.com/tx/${hash}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 underline ml-1"
-          >
-            View on BscScan
-          </a>
-        </div>
-      );
-    }
-  }, [hash]);
-
-  const handleInvest = () => {
-    writeContract({
-      abi: byForexConfig.abi,
-      address: byForexConfig.address as `0x${string}`,
-      functionName: 'invest',
-      args: [BigInt(packageId), BigInt(investmentAmount * 1e18)],
-    });
-  };
-
-  const handleApprove = () => {
-    writeContract({
-      address: USDT_ADDRESS,
-      abi: parseAbi(['function approve(address spender, uint256 amount)']),
-      functionName: 'approve',
-      args: [byForexConfig.address as `0x`, BigInt(investmentAmount * 1e18)],
-    })
-    setIsApproved(true);
-    if(isApproved){
-      location.reload();
-    }
-  }
-
-  const handlePoolClaim = (poolId: number) => {
-    writeContract({
-      abi: byForexConfig.abi,
-      address: byForexConfig.address as `0x${string}`,
-      functionName: 'claimPoolShare',
-      args: [BigInt(poolId)],
-    });
-  };
-  const handleIncomeClaim = (amount: number) => {
-    writeContract({
-      abi: byForexConfig.abi,
-      address: byForexConfig.address as `0x${string}`,
-      functionName: 'withdraw',
-      args: [BigInt(amount)],
-    });
-  };
 
   return (
     <div className="px-3 md:px-28 py-20 flex flex-col ">
@@ -154,8 +25,8 @@ const Investments = () => {
               {packages.map((item, index) => (
                 <p
                   key={index}
-                  onClick={() => { setInvestmentAmount(item); setPackageId(index + 1); }}
-                  className={`text-black font-semibold p-4 cursor-pointer ${investmentAmount === item ? "bg-neutral-400" : "bg-gray-200"
+                  // onClick={() => { setInvestmentAmount(item); setPackageId(index + 1); }}
+                  className={`text-black font-semibold p-4 cursor-pointer bg-neutral-400
                     }`}
                 >
                   ${item}
@@ -165,19 +36,20 @@ const Investments = () => {
             </div>
             <div className="flex bg-neutral-200 rounded-md p-2 justify-between">
               <p>Total investments</p>
-              <p className="text-primary">{userInfo ? formatBigInt(userInfo[2]) : 0}</p>
+              {/* <p className="text-primary">{userInfo ? formatBigInt(userInfo[2]) : 0}</p> */}
             </div>
             <button
-              onClick={!isApproved ? handleApprove : handleInvest}
-              disabled={isConfirming || investmentAmount === 0}
-              className={`bg-primary w-full py-2 rounded-lg text-lg font-semibold text-white outline-none ${isConfirming ? 'opacity-50 cursor-not-allowed' : ''
+              // onClick={!isApproved ? handleApprove : handleInvest}
+              // disabled={isConfirming || investmentAmount === 0}
+              className={`bg-primary w-full py-2 rounded-lg text-lg font-semibold text-white outline-none opacity-50 cursor-not-allowed
                 }`}
             >
-              {isConfirming
+              {/* {isConfirming
                 ? 'Confirming...'
                 : !isApproved
                   ? `Approve ${formatAmount(investmentAmount)} USDT`
-                  : `Invest ${formatAmount(investmentAmount)} USDT`}
+                  : `Invest ${formatAmount(investmentAmount)} USDT`} */}
+                  Approve
             </button>
           </div>
         </div>
@@ -190,11 +62,12 @@ const Investments = () => {
                 <div key={poolId} className="bg-neutral-200 flex justify-between p-2 rounded-lg">
                   <p className="text-lg font-semibold my-auto">Pool {poolId}</p>
                   <p className="text-primary">
-                    ${dashInfo?.poolsClaim ? formatBigInt(dashInfo.poolsClaim[poolId - 1]) : 0}
+                    {/* ${dashInfo?.poolsClaim ? formatBigInt(dashInfo.poolsClaim[poolId - 1]) : 0} */}
+                    0
                   </p>
                   <button
                     className="rounded-lg border-2 border-primary text-primary py-1 px-3 font-semibold"
-                    onClick={() => handlePoolClaim(poolId)}
+                    // onClick={() => handlePoolClaim(poolId)}
                   >
                     Claim
                   </button>
@@ -210,13 +83,15 @@ const Investments = () => {
             <div className=" bg-white w-full rounded-lg py-5 px-3 flex flex-col gap-5 ">
               <div className="flex justify-between">
                 <p className="font-bold text-lg">Total income claim</p>
-                <p className="text-primary">${dashInfo?.totalIncomeClaim ? formatBigInt(dashInfo?.totalIncomeClaim) : 0}</p>
+                <p className="text-primary">$100</p>
+                {/* <p className="text-primary">${dashInfo?.totalIncomeClaim ? formatBigInt(dashInfo?.totalIncomeClaim) : 0}</p> */}
               </div>
               <div className="flex justify-between">
                 <p className="font-bold text-lg">Available income claim</p>
-                <p className="text-primary">${dashInfo?.availableIncomeClaim ? formatBigInt(dashInfo?.availableIncomeClaim) : 0}</p>
+                <p className="text-primary">0</p>
+                {/* <p className="text-primary">${dashInfo?.availableIncomeClaim ? formatBigInt(dashInfo?.availableIncomeClaim) : 0}</p> */}
               </div>
-              <div className="flex w-full justify-end"><button className="text-white text-xl font-semibold bg-primary w-fit py-1 px-4 rounded-md" onClick={() => { handleIncomeClaim(1) }}>Claim</button></div>
+              <div className="flex w-full justify-end"><button className="text-white text-xl font-semibold bg-primary w-fit py-1 px-4 rounded-md">Claim</button></div>
             </div>
           </div>
         </div>
@@ -228,11 +103,11 @@ const Investments = () => {
               {/* <div className="w-full border-2 rounded-md border-black h-12 text-lg text-center outline-none font-semibold flex items-center justify-center">
                 {generateReferralLink()}
               </div> */}
-              <input type="text" className="h-12 border-2  border-black rounded-lg w-full px-3 text-center font-semibold" value={generateReferralLink()} />
+              <input type="text" className="h-12 border-2  border-black rounded-lg w-full px-3 text-center font-semibold"/>
             </div>
             <div className="flex w-full justify-end">
               <button
-                onClick={() => navigator.clipboard.writeText(generateReferralLink())}
+                // onClick={() => navigator.clipboard.writeText(generateReferralLink())}
                 className="text-white text-xl font-semibold bg-primary w-full md:w-fit py-2 px-4 rounded-md"
               >
                 Copy
